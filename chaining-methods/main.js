6bbe271486c7c402e94d40e24999b6f3ee1f8da8
Cole Doster <wcdoster@gmail.com>
2018-04-24 10:09:01 -0500

    fixed the filter method

--- chaining-methods/main.js
+++ chaining-methods/main.js
@@ -7,9 +7,8 @@ const integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
 console.log(integers.sort((a, b) => {return b-a}).filter(
     integer => {
-        if (integer < 19) {
-            return integer;
-        }
+        const filteredInteger = (integer < 19) 
+        return filteredInteger;
     }
 ).map(
     integer => {
